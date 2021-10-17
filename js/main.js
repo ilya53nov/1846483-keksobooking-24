cd3fce17334256ef90e407a272a1e4d04ad99441
ilya53nov <ilya53nov@yandex.ru>
2021-10-17 20:55:30 +0300

    Импортированы функции активации и деактивации форм

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import {advertisementListFragment} from './popup.js';
+import {activateForms, disableForms} from './form.js';
+
+activateForms();
 
 const similarElement = document.querySelector('#map-canvas');
 
