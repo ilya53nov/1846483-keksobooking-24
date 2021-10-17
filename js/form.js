71f1ee259453149b09ae873a0fad497e6b46fe14
ilya53nov <ilya53nov@yandex.ru>
2021-10-17 20:54:31 +0300

    Добавлен модуль для работы с формой

---
+++ js/form.js
@@ -0,0 +1,27 @@
+const formAdInformation = document.querySelector('.ad-form');
+
+const formFilters = document.querySelector('.map__filters');
+
+const switchFormState = (isDisabled, ...forms) => {
+  forms.forEach((form) => {
+    const classNameForm = form.className;
+    form.classList.remove(classNameForm);
+
+    if(isDisabled){
+      form.classList.add(`${classNameForm}--disabled`);
+    }
+    else{
+      form.classList.add(classNameForm.replace('--disabled',''));
+    }
+
+    form.childNodes.forEach((element) => element.disabled = isDisabled);
+  });
+};
+
+const disableForms = () => switchFormState(true, formAdInformation, formFilters);
+
+export{disableForms};
+
+const activateForms = () => switchFormState(false, formAdInformation, formFilters);
+
+export{activateForms};
