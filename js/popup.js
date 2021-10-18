56cdf0466f95bb2d45f9f5f1dd46200138c098ee
ilya53nov <ilya53nov@yandex.ru>
2021-10-18 16:29:02 +0300

    Рефакторинг

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,8 @@
 import {createAdvertisements} from './data.js';
 import {declension} from './util.js';
 
+const HIDDEN_CLASS = 'hidden';
+
 const TYPES = {
   flat: 'Квартира',
   bungalow: 'Бунгало',
@@ -9,7 +11,14 @@ const TYPES = {
   hotel: 'Отель',
 };
 
-const checkValueAndAddTextContentOrClass = (element, value) => (value) ? element.textContent = value : element.classList.add('hidden)');
+const checkValueAndAddTextContentOrClassHidden = (element, checkValue, insertValue) => {
+  if(checkValue){
+    element.textContent = insertValue;
+  }
+  else{
+    element.classList.add(HIDDEN_CLASS);
+  }
+};
 
 const similarAdvertisementTemlate = document.querySelector('#card')
   .content
@@ -22,42 +31,70 @@ const advertisementListFragment = document.createDocumentFragment();
 similarAdvertisements.forEach(({offer, author}) => {
   const advertisementElement = similarAdvertisementTemlate.cloneNode(true);
 
-  advertisementElement.querySelector('.popup__title').textContent = offer.title;
-  advertisementElement.querySelector('.popup__text--address').textContent = offer.address;
-  advertisementElement.querySelector('.popup__text--price').innerHTML = `${offer.price} <span>₽/ночь</span>`;
-  advertisementElement.querySelector('.popup__type').textContent = TYPES[offer.type];
+  checkValueAndAddTextContentOrClassHidden(advertisementElement.querySelector('.popup__title'), offer.title, offer.title);
+  checkValueAndAddTextContentOrClassHidden(advertisementElement.querySelector('.popup__text--address'), offer.address, offer.address);
+  checkValueAndAddTextContentOrClassHidden(advertisementElement.querySelector('.popup__type'), offer.type, TYPES[offer.type]);
+  checkValueAndAddTextContentOrClassHidden(advertisementElement.querySelector('.popup__text--time'), offer.checkin && offer.checkout, `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`);
+  checkValueAndAddTextContentOrClassHidden(advertisementElement.querySelector('.popup__description'), offer.description, offer.description);
 
-  const rooms = declension(['комната', 'комнаты', 'комнат'], offer.rooms);
-  const quests = declension(['гостя', 'гостей', 'гостей'], offer.quests);
+  if(offer.price){
+    advertisementElement.querySelector('.popup__text--price').textContent = offer.price;
+    const spanElement = document.createElement('span');
+    spanElement.textContent = ' ₽/ночь';
+    advertisementElement.querySelector('.popup__text--price').appendChild(spanElement);
+  }
+  else{
+    advertisementElement.querySelector('.popup__text--price').classList.add(HIDDEN_CLASS);
+  }
 
-  advertisementElement.querySelector('.popup__text--capacity').textContent = `${rooms} для ${quests}`;
-  advertisementElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+  if (offer.rooms && offer.quests){
+    const rooms = declension(['комната', 'комнаты', 'комнат'], offer.rooms);
+    const quests = declension(['гостя', 'гостей', 'гостей'], offer.quests);
+    advertisementElement.querySelector('.popup__text--capacity').textContent = `${rooms} для ${quests}`;
+  }
+  else{
+    advertisementElement.querySelector('.popup__text--capacity').classList.add(HIDDEN_CLASS);
+  }
 
-  const featuresContainer = advertisementElement.querySelector('.popup__features');
-  featuresContainer.innerHTML = '';
+  if (offer.features){
+    const featuresContainer = advertisementElement.querySelector('.popup__features');
+    featuresContainer.innerHTML = '';
 
-  offer.features.forEach((element) => {
-    const createElementFeatures = document.createElement('li');
-    createElementFeatures.classList.add('popup__feature');
-    createElementFeatures.classList.add(`popup__feature--${element}`);
-    featuresContainer.appendChild(createElementFeatures);
-  });
+    offer.features.forEach((element) => {
+      const createElementFeatures = document.createElement('li');
+      createElementFeatures.classList.add('popup__feature');
+      createElementFeatures.classList.add(`popup__feature--${element}`);
+      featuresContainer.appendChild(createElementFeatures);
+    });
+  }
+  else{
+    advertisementElement.querySelector('.popup__features').classList.add(HIDDEN_CLASS);
+  }
 
-  checkValueAndAddTextContentOrClass(advertisementElement.querySelector('.popup__description'), offer.description);
+  if(offer.photos){
+    const photoContainer = advertisementElement.querySelector('.popup__photos');
+    const photoListFragment = document.createDocumentFragment();
 
-  const photoContainer = advertisementElement.querySelector('.popup__photos');
-  const photoListFragment = document.createDocumentFragment();
+    offer.photos.forEach((photoItem) => {
+      const photoElement = photoContainer.querySelector('.popup__photo').cloneNode(true);
+      photoElement.src = photoItem;
+      photoListFragment.appendChild(photoElement);
+    });
 
-  offer.photos.forEach((photoItem) => {
-    const photoElement = photoContainer.querySelector('.popup__photo').cloneNode(true);
-    photoElement.src = photoItem;
-    photoListFragment.appendChild(photoElement);
-  });
+    photoContainer.innerHTML ='';
+    photoContainer.appendChild(photoListFragment);
+  }
+  else{
+    advertisementElement.querySelector('.popup__photos').classList.add(HIDDEN_CLASS);
+  }
 
-  photoContainer.innerHTML ='';
-  photoContainer.appendChild(photoListFragment);
+  if(author.avatar){
+    advertisementElement.querySelector('.popup__avatar').src = author.avatar;
+  }
+  else{
+    advertisementElement.querySelector('.popup__avatar').classList.add(HIDDEN_CLASS);
+  }
 
-  advertisementElement.querySelector('.popup__avatar').src = author.avatar;
   advertisementListFragment.appendChild(advertisementElement);
 });
 
