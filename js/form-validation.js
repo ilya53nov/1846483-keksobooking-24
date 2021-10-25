384e23355cdf60cfb55430de2cd3bfcc0e3f1698
ilya53nov <ilya53nov@yandex.ru>
2021-10-25 21:01:08 +0300

    Добавлены новые поля для валидации

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,4 +1,4 @@
-import { declension } from './util.js';
+import { getDeclension } from './util.js';
 
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
@@ -7,92 +7,116 @@ const MAX_PRICE_VALUE = 1000000;
 // Валидация поля "Заголовок объявления"
 const titleInput = document.querySelector('#title');
 
-titleInput.addEventListener('input',() => {
+const onTitleInputChange = () => {
   const valueLength = titleInput.value.length;
 
-  if(valueLength < MIN_TITLE_LENGTH){
-    titleInput.setCustomValidity(`Ёще ${declension(['символ', 'символа', 'символов'], MIN_TITLE_LENGTH - valueLength)}`);
-  } else if (valueLength > MAX_TITLE_LENGTH){
-    titleInput.setCustomValidity(`Удалите лишние ${declension(['символ', 'символа', 'символов'],valueLength - MAX_TITLE_LENGTH)}`);
-  } else{
+  if (valueLength < MIN_TITLE_LENGTH) {
+    titleInput.setCustomValidity(`Ёще ${getDeclension(['символ', 'символа', 'символов'], MIN_TITLE_LENGTH - valueLength)}`);
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    titleInput.setCustomValidity(`Удалите лишние ${getDeclension(['символ', 'символа', 'символов'],valueLength - MAX_TITLE_LENGTH)}`);
+  } else {
     titleInput.setCustomValidity('');
   }
 
   titleInput.reportValidity();
-});
+};
+
+titleInput.addEventListener('input', onTitleInputChange);
 
 // Валидация поля "Цена за ночь"
 const priceInput = document.querySelector('#price');
+const typeInput = document.querySelector('#type');
 
-priceInput.addEventListener('input', () => {
+const rulesMinPrice = {
+  'Бунгало' : 0,
+  'Квартира' : 1000,
+  'Отель' : 3000,
+  'Дом' : 5000,
+  'Дворец' : 10000,
+};
+
+const getMinPrice = () => rulesMinPrice[typeInput.selectedOptions[0].text];
+
+const onPriceInputChange = () => {
   const value = priceInput.value;
 
-  if(value > MAX_PRICE_VALUE){
+  if (value < getMinPrice()) {
+    priceInput.setCustomValidity(`Не верно указана цена для типа жилья "${typeInput.selectedOptions[0].text}", минимальая цена: ${getMinPrice()}`);
+  } else if (value > MAX_PRICE_VALUE) {
     priceInput.setCustomValidity(`Указанное значение больше максимального, максимальное: ${MAX_PRICE_VALUE}`);
-  }else{
+  } else {
     priceInput.setCustomValidity('');
   }
 
   priceInput.reportValidity();
-});
+};
+
+const onTypeInputChange = () => {
+  priceInput.min = getMinPrice();
+  priceInput.placeholder = getMinPrice();
+  onPriceInputChange();
+};
+
+typeInput.addEventListener('input', onTypeInputChange);
+
+priceInput.addEventListener('input', onPriceInputChange);
 
 // Валидация поля "Количество комнат и количество мест"
-
 const roomInput = document.querySelector('#room_number');
 const capacityInput = document.querySelector('#capacity');
 
-const rules = {
-  '1': ['1'],
-  '2': ['1', '2'],
-  '3': ['1', '2', '3'],
-  '100': ['0'],
+const rulesCapacityInput = {
+  '1 комната': ['для 1 гостя'],
+  '2 комнаты': ['для 1 гостя', 'для 2 гостей'],
+  '3 комнаты': ['для 1 гостя', 'для 2 гостей', 'для 3 гостей'],
+  '100 комнат': ['не для гостей'],
 };
 
-const isValidCapacityInput = () => {
-  if(rules[roomInput.value].some((element) =>  element === capacityInput.value)){
-    return true;
-  }else{
-    return false;
-  }
-};
+const isValidCapacityInput = () => rulesCapacityInput[roomInput.selectedOptions[0].text].some((element) => element === capacityInput.selectedOptions[0].text);
 
-const createMessageCustomValidity = () => {
-  let messageError = 'Не верно указано количество мест, необходимо выбрать: ';
-  rules[roomInput.value].forEach((element) => {
-    if(element === '0'){
-      messageError += 'не для гостей';
-    }else{
-      if(rules[roomInput.value].indexOf(element) > 0){
-        messageError += ' или ';
-      }
-      messageError += `для ${declension(['гостя', 'гостей', 'гостей'], element)}`;
-    }
-  });
-  return `${messageError}.`;
-};
+const createMessageCustomValidity = () => `Не верно указано количество мест, необходимо выбрать: ${rulesCapacityInput[roomInput.selectedOptions[0].text].join(' или ')}.`;
 
 const onOptionChange = () => {
-  if (!isValidCapacityInput()){
+  if (!isValidCapacityInput()) {
     capacityInput.setCustomValidity(createMessageCustomValidity());
-  }else{
+  } else {
     capacityInput.setCustomValidity('');
   }
+
   capacityInput.reportValidity();
 };
 
 capacityInput.addEventListener('change', onOptionChange);
-
 roomInput.addEventListener('change', onOptionChange);
 
-const form = document.querySelector('.ad-form');
+// Валидация полей "Время заезда", "Время выезда"
+const timeInInput = document.querySelector('#timein');
+const timeOutInput = document.querySelector('#timeout');
 
+const onTimeInInputChange = () => timeOutInput.value = timeInInput.value;
+const onTimeOutInputChange = () => timeInInput.value = timeOutInput.value;
+
+timeInInput.addEventListener('input', onTimeInInputChange);
+timeOutInput.addEventListener('input', onTimeOutInputChange);
+
+// Валидация поля адрес
 const addressInput = document.querySelector('#address');
+addressInput.readOnly = true;
+
+// Валидация полей загрузки фото
+const avatarInput = document.querySelector('#avatar');
+avatarInput.accept = 'image/*';
+
+const imagesInput = document.querySelector('#images');
+imagesInput.accept = 'image/*';
+
+const form = document.querySelector('.ad-form');
 
 form.addEventListener('submit', (evt) => {
   //Добавление временного значения в поле адрес (для успешной отправки формы)
   addressInput.value = '55.55';
 
-  if(!isValidCapacityInput()){
+  if (!isValidCapacityInput()) {
     onOptionChange();
     evt.preventDefault();
     return false;
