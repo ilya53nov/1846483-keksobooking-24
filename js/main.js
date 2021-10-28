e6c07669466cf144cbeab5a70965a3a8b1cd8575
ilya53nov <ilya53nov@yandex.ru>
2021-10-28 22:27:09 +0300

    Отрисовка карты с похожими объявлениями

--- js/main.js
+++ js/main.js
@@ -1,9 +1,73 @@
-import {renderSimilarList} from './similar-list.js';
+import {renderSimilarAdvertisement} from './similar-list.js';
+import {createAdvertisements} from './data.js';
 import {activateForms, disableForms} from './form.js';
 import './form-validation.js';
+import {addressInput} from './form-validation.js';
 
-//disableForms();
+disableForms();
 
-const similarElement = document.querySelector('#map-canvas');
+const coordinateTokyo = {
+  lat: 35.68950,
+  lng: 139.69200,
+};
 
-similarElement.appendChild(renderSimilarList.firstChild);
+addressInput.value = `${coordinateTokyo.lat}, ${coordinateTokyo.lng}`;
+
+const mapCanvas = document.querySelector('#map-canvas');
+
+const map = L.map(mapCanvas)
+  .on('load', () => {
+    activateForms();
+  })
+  .setView(coordinateTokyo, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAncor: [26, 52],
+});
+
+const icon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAncor: [20, 40],
+});
+
+const mainMarker = L.marker(
+  {
+    lat: coordinateTokyo.lat,
+    lng: coordinateTokyo.lng,
+  },
+  {
+    icon: mainPinIcon,
+    draggable: true,
+  },
+).addTo(map);
+
+const createMarker = (element) => {
+  const marker = L.marker(element.location, icon)
+    .addTo(map)
+    .bindPopup(renderSimilarAdvertisement(element))
+    .on('click', () => marker.openPopup());
+};
+
+const similarAdvertisements = createAdvertisements();
+
+similarAdvertisements.forEach((element) => {
+  createMarker(element);
+});
+
+const currentCoordinate = (evt) => evt.target.getLatLng();
+
+const setValueAddressInput = (evt) => {
+  addressInput.value = `${currentCoordinate(evt).lat.toFixed(5)}, ${currentCoordinate(evt).lng.toFixed(5)}`;
+};
+
+mainMarker.on('moveend', setValueAddressInput);
