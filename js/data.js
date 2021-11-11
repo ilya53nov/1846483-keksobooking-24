f59fd48cba499f15c167ef95bcee36e666f418f7
ilya53nov <ilya53nov@yandex.ru>
2021-11-11 19:56:32 +0300

    Добавления модуля для хранения данных о типе жилья

---
+++ js/data.js
@@ -0,0 +1,28 @@
+const types = {
+  flat: {
+    rusLocalization  : 'Квартира',
+    minPrice : 1000,
+  },
+
+  bungalow: {
+    rusLocalization  : 'Бунгало',
+    minPrice : 0,
+  },
+
+  house: {
+    rusLocalization  : 'Дом',
+    minPrice : 5000,
+  },
+
+  palace: {
+    rusLocalization  : 'Дворец',
+    minPrice : 10000,
+  },
+
+  hotel: {
+    rusLocalization  : 'Отель',
+    minPrice : 3000,
+  },
+};
+
+export {types};
